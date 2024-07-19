310d27d8335d6b82c5489d16eb4853ba4272bffe
afsaanchez04 <afsanchez04@gmail.com>
2024-07-19 15:25:22 -0500

    feat: boton-cerrar

--- 3-dinamic-dom/app.js
+++ 3-dinamic-dom/app.js
@@ -8,20 +8,28 @@ let n = 1;
 function crear (){
     
     const ball = document.createElement('div');
+    const equis = document.createElement('button');
+
+    equis.innerText = 'x';
+    equis.classList.add('equis');
+
     ball.classList.add('pelotica');
     ball.style.backgroundColor = listaColores.value;
     ball.innerText = n++;
+/*     ball.innerHTML = equis; */
+    ball.appendChild(equis)
 
     main.appendChild(ball);
-    console.log(ball)
 }
 
 function borrarPantalla (){
     main.innerHTML = '';
     n=1;
 }
-function borrarElemento (e){
-    console.log('Le di click a la pelotica: ' + e.target.innerText)
+function borrarElemento (e){ 
+    if(e.target.innerText == 'x'){
+        main.removeChild(e.target.parentElement);
+    }
 }
 
 btnCrear.addEventListener('click',crear)
