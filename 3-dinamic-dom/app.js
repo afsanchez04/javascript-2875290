f81aa1aba4b2e7236a641b9c53776b658c0e25fd
afsaanchez04 <afsanchez04@gmail.com>
2024-07-19 14:55:50 -0500

    feat: borrar-elementos

---
+++ 3-dinamic-dom/app.js
@@ -0,0 +1,30 @@
+const listaColores = document.querySelector('#listaColores');
+const btnCrear = document.querySelector('#btnCrear');
+const main = document.querySelector('#main');
+const btnBorrar = document.querySelector('#btnBorrar');
+let n = 1;
+
+
+function crear (){
+    
+    const ball = document.createElement('div');
+    ball.classList.add('pelotica');
+    ball.style.backgroundColor = listaColores.value;
+    ball.innerText = n++;
+
+    main.appendChild(ball);
+    console.log(ball)
+}
+
+function borrarPantalla (){
+    main.innerHTML = '';
+    n=1;
+}
+function borrarElemento (e){
+    console.log('Le di click a la pelotica: ' + e.target.innerText)
+}
+
+btnCrear.addEventListener('click',crear)
+btnBorrar.addEventListener('click',borrarPantalla)
+main.addEventListener('click',borrarElemento)
+
