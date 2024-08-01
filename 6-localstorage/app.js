2f6034643df9606661f9d387bdd0d822ac0dff6b
afsaanchez04 <afsanchez04@gmail.com>
2024-08-01 15:05:08 -0500

    feat: inicio sesión

--- 6-localstorage/app.js
+++ 6-localstorage/app.js
@@ -1,4 +1,51 @@
 
+
+const formulario = document.querySelector('.formulario')
+const username = document.querySelector('.username')
+const password = document.querySelector('.password')
+
+//Registrar el dato de "user" en localstorage
+let user = {
+    userName: 'admin',
+    userPass: '1234',
+}
+
+localStorage.setItem('user', JSON.stringify(user) )
+
+//Inicio de sesión
+function validarUsuario (e){
+    e.preventDefault(); 
+
+    let currentUser = JSON.parse( localStorage.getItem('user') )
+    console.log(currentUser.userName)
+    console.log(currentUser.userPass)
+
+    console.log(Boolean(username.value === currentUser.userName)) ;
+    console.log(Boolean(password.value === currentUser.userPass)) ;
+
+    if(username.value === currentUser.userName && password.value === currentUser.userPass){
+        console.log('Felicidades puedes entrar 🔥')
+        window.location = "./userpage.html"
+    }else{
+        console.log('Sigue intentando')
+    }
+
+    formulario.reset()
+}
+formulario.addEventListener('submit',validarUsuario)
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* let nombre = 'José'
 let items = ['uno','dos','tres','cuatro','cinco']
 let pelicula = {
