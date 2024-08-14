4122f451629506c8e74c922c942188d855de1381
afsaanchez04 <afsanchez04@gmail.com>
2024-08-14 15:47:35 -0500

    feat: buscador version 1

--- 8-fetch-api/app.js
+++ 8-fetch-api/app.js
@@ -1,28 +1,19 @@
-const container = document.querySelector('.container')
+const container = document.querySelector(".container")
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users/')
   .then(respuesta => respuesta.json())
   .then(datos => {
     datos.forEach(element => {
+        let box = document.createElement('div')
+        box.classList.add("box");
+        container.appendChild(box)
 
-        console.log(`Name: ${element.name}`)
-        console.log(`Username: ${element.username}`)
+        const title = document.createElement("h1")
+        title.innerText = `Name: ${element.name}`
+        box.appendChild(title)
 
-        //Con JavaScript: Crear un elemento "div" con createElement y asignarle la clase "box"
-        //Crear un "h1" con createElement y hacer appendChild al div de clase "box"
-        //Crear un "p" con createElement y hacer appendChild al div de clase "box"
-
-        const box = document.createElement('div');
-        box.classList.add('box');
-
-        const h1 = document.createElement('h1');
-        h1.textContent = element.name;
-        box.appendChild(h1);
-
-        const p = document.createElement('p');
-        p.textContent = element.username;
-        box.appendChild(p);
-
-        container.appendChild(box);
+        const text = document.createElement("p")
+        text.innerText = `Userame: ${element.username}`
+        box.appendChild(text)
     });
   })
--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -1,19 +1,101 @@
-const container = document.querySelector('.container')
+const container = document.querySelector(".container")
+const buscador = document.querySelector("#buscador")
+const textoBusqueda = document.querySelector("#textoBusqueda")
+const listaStatus = document.querySelector("#listaStatus")
+let results;
 
-let url = 'https://rickandmortyapi.com/api/character'
+function pintar(elemento) {
+
+  switch (elemento) {
+    case 'Alive':
+      return 'green'
+      break
+    case 'Dead':
+      return 'red'
+      break
+    default:
+      return 'gray'
+  }
+
+}
+
+function filtrar(e) {
+
+  e.preventDefault()
+
+  container.innerHTML = ""
+
+  const filtro = results.filter( word => word.status == listaStatus.value);
+
+  console.log(filtro)
+
+  filtro.forEach(element => {
+
+    const card = document.createElement('div')
+    card.classList.add("card");
+    container.appendChild(card)
+
+    card.innerHTML = ` 
+          <img src="${element.image}" width= "200px">
+          <div class="info">
+          <h1>${element.name}</h1>
+          <div class="section">
+          
+          <div class="circle ${pintar(element.status)}"></div>
+          <span class="estado">${element.status} - ${element.species} </span>
+          </div>
+          <div class="detalle">
+              <p><strong>Última ubicación conocida:</strong><br>${element.location.name}</p>
+              <p><strong>Visto por primera vez en:</strong><br>${element.origin.name}</p>
+          </div>
+      </div>
+      </div>
+      `
+    container.appendChild(card)
+  })
+}
+
+buscador.addEventListener('submit',filtrar)
+
+
+let url = "https://rickandmortyapi.com/api/character"
 fetch(url)
-.then( response => response.json() )
-.then( data => {
+  .then(response => response.json())
+  .then(data => {
 
-    data.results.forEach(element => {
-        const box = document.createElement('div')
-        box.classList.add("box")
+    results = data.results;
 
-        box.innerHTML = `
-            <img src="${element.image}" width="200px">
-            <p>${element.name}</p>
-        `
-        container.appendChild(box)
-    });
+    /*  const result = data.results.filter( word => word.status == "Alive");
+     console.log(result)
+   
+       data.results.forEach(element => {
+           //console.log(data.results)
+           const card = document.createElement('div')
+           card.classList.add("card");
+           container.appendChild(card)
+   
+           card.innerHTML = ` 
+               <img src="${element.image}" width= "200px">
+               <div class="info">
+               <h1>${element.name}</h1>
+               <div class="section">
+               
+               <div class="circle ${pintar(element.status)}"></div>
+               <span class="estado">${element.status} - ${element.species} </span>
+               </div>
+               <div class="detalle">
+                   <p><strong>Última ubicación conocida:</strong><br>${element.location.name}</p>
+                   <p><strong>Visto por primera vez en:</strong><br>${element.origin.name}</p>
+               </div>
+           </div>
+           </div>
+           `
+           container.appendChild(card)
+   
+        
+    }); */
+  }
+  )
 
-} )
+
+
