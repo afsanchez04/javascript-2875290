a527ee313647a35d359dec2747d71f15908a69ef
afsaanchez04 <afsanchez04@gmail.com>
2024-08-15 15:01:19 -0500

    feat: buscador-terminado

--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -4,6 +4,15 @@ const textoBusqueda = document.querySelector("#textoBusqueda")
 const listaStatus = document.querySelector("#listaStatus")
 let results;
 
+let url = "https://rickandmortyapi.com/api/character"
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    results = data.results;
+    mostrarResults(data.results)
+  }
+  )
+
 function pintar(elemento) {
 
   switch (elemento) {
@@ -19,17 +28,8 @@ function pintar(elemento) {
 
 }
 
-function filtrar(e) {
-
-  e.preventDefault()
-
-  container.innerHTML = ""
-
-  const filtro = results.filter( word => word.status == listaStatus.value);
-
-  console.log(filtro)
-
-  filtro.forEach(element => {
+function mostrarResults(result) {
+  result.forEach(element => {
 
     const card = document.createElement('div')
     card.classList.add("card");
@@ -55,47 +55,35 @@ function filtrar(e) {
   })
 }
 
-buscador.addEventListener('submit',filtrar)
+function filtrar(e) {
 
+  e.preventDefault()
 
-let url = "https://rickandmortyapi.com/api/character"
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
+  container.innerHTML = ""
 
-    results = data.results;
+  if(listaStatus.value != "all"){
+    const filtro = results.filter(word => word.status == listaStatus.value).filter( word => word.name.toLowerCase().includes(textoBusqueda.value.toLowerCase()) );
+    //textoBusqueda.value.toLowerCase() == word.name.toLowerCase()
 
-    /*  const result = data.results.filter( word => word.status == "Alive");
-     console.log(result)
-   
-       data.results.forEach(element => {
-           //console.log(data.results)
-           const card = document.createElement('div')
-           card.classList.add("card");
-           container.appendChild(card)
-   
-           card.innerHTML = ` 
-               <img src="${element.image}" width= "200px">
-               <div class="info">
-               <h1>${element.name}</h1>
-               <div class="section">
-               
-               <div class="circle ${pintar(element.status)}"></div>
-               <span class="estado">${element.status} - ${element.species} </span>
-               </div>
-               <div class="detalle">
-                   <p><strong>Última ubicación conocida:</strong><br>${element.location.name}</p>
-                   <p><strong>Visto por primera vez en:</strong><br>${element.origin.name}</p>
-               </div>
-           </div>
-           </div>
-           `
-           container.appendChild(card)
-   
-        
-    }); */
+    mostrarResults(filtro)
+  }else{
+    //""-> false    "content"->true
+    if(textoBusqueda.value){
+
+      const filtro = results.filter( word => word.name.toLowerCase().includes(textoBusqueda.value.toLowerCase()) );
+      mostrarResults(filtro)
+
+    }else{
+      mostrarResults(results)
+    }
+
+    textoBusqueda.value = ""
   }
-  )
+}
+
+buscador.addEventListener('submit', filtrar)
+
+
 
 
 
